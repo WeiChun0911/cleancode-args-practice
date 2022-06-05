2636c5363e915357e41b2cfd6c681a63107d20ba
WillyLiu <willywcliu@kknow.com>
2022-06-05 10:10:33 +0800

    開始著手把 set 方法移進 AM class, 第一步是移除 if else
    These were simple changes that kept all the tests passing.
    Now we can start moving the set functions down into the appropriate derivatives.
     First, I need to make the following change in setArgument:
    
    This change is important because we want to completely eliminate the if-else chain.
    Therefore, we needed to get the error condition out of it.

--- src/main/java/com/objectmentor/utilities/args/firstdraft/booleanstringandinteger/Args.java
+++ src/main/java/com/objectmentor/utilities/args/firstdraft/booleanstringandinteger/Args.java
@@ -109,6 +109,8 @@ public class Args {
 
     private boolean setArgument(char argChar) throws ArgsException {
         ArgumentMarshaler m = marshalers.get(argChar);
+        if (m == null)
+            return false;
         try {
             if (m instanceof IntegerArgumentMarshaler)
                 setBooleanArg(m);
@@ -116,8 +118,6 @@ public class Args {
                 setStringArg(m);
             else if (m instanceof IntegerArgumentMarshaler)
                 setIntArg(m);
-            else
-            return false;
         } catch (ArgsException e) {
             valid = false;
             errorArgumentId = argChar;
