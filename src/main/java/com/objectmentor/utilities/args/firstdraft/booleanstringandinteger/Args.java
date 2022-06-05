1ccb886c691f45abfe9d7767770089073378346a
WillyLiu <willywcliu@kknow.com>
2022-06-05 10:38:47 +0800

    加上 Double 的 AM
    Next, we write the DoubleArgumentMarshaler class.

--- src/main/java/com/objectmentor/utilities/args/firstdraft/booleanstringandinteger/Args.java
+++ src/main/java/com/objectmentor/utilities/args/firstdraft/booleanstringandinteger/Args.java
@@ -246,4 +246,27 @@ public class Args {
             return intValue;
         }
     }
+
+    private class DoubleArgumentMarshaler implements ArgumentMarshaler {
+        private double doubleValue = 0;
+
+        public void set(Iterator<String> currentArgument) throws ArgsException {
+            String parameter = null;
+            try {
+                parameter = currentArgument.next();
+                doubleValue = Double.parseDouble(parameter);
+            } catch (NoSuchElementException e) {
+                errorCode = ErrorCode.MISSING_DOUBLE;
+                throw new ArgsException();
+            } catch (NumberFormatException e) {
+                errorParameter = parameter;
+                errorCode = ErrorCode.INVALID_DOUBLE;
+                throw new ArgsException();
+            }
+        }
+
+        public Object get() {
+            return doubleValue;
+        }
+    }
 }
